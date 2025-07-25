fe473afeceb6b6d1b0d41c7160a2a3cdcb1e7e72
Sithum Sathsara <89584440+SithumSathsaras@users.noreply.github.com>
2025-07-25 08:29:29 -0400

    python path add

--- terminalos/__main__.py
+++ terminalos/__main__.py
@@ -1,13 +1,4 @@
 from .cli import main
 
-from .core.app import TerminalOSApp
-
-
-def main():
-    """Entry point for terminalos command"""
-    app = TerminalOSApp()
-    app.run()
-
-
 if __name__ == "__main__":
     main()
